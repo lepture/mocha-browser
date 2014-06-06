0ed5eff23fbf550ef96e80fd0fbfb0e222e1b691
afc163 <afc163@gmail.com>
2014-06-06 17:23:46 +0800

    better preinstall phantomjs

--- preinstall.js
+++ preinstall.js
@@ -1,15 +1,15 @@
 var exec = require('child_process').exec;
 var os = require('os');
-var install;
 
-if (os.platform() === 'win32') {
-  exec('set PHANTOMJS_CDNURL=http://phantomjs.qiniudn.com&& npm install phantomjs',
-    function (error, stdout, stderr) {
-      console.log(stdout);
-    });
-} else {
-  exec('PHANTOMJS_CDNURL=http://phantomjs.qiniudn.com npm install phantomjs',
-    function (error, stdout, stderr) {
-      console.log(stdout);
-    });
-}
+process.env.PHANTOMJS_CDNURL = "http://phantomjs.qiniudn.com";
+var install = exec('npm install phantomjs', {
+  env: process.env
+});
+
+install.stdout.on('data', function(data) {
+  process.stdout.write(data);
+});
+
+install.stderr.on('data', function(data) {
+  process.stdout.write(data);
+});
